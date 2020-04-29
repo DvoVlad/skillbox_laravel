abfeaee7992d0606e549077cc553d6db9b04edd2
Vladislav Dvoryaninov <dvovlad@mail.ru>
2020-04-29 12:07:40 +0300

    Edit menu

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -4,6 +4,8 @@
       <a class="p-2 text-muted" href="/about">О нас</a>
       <a class="p-2 text-muted" href="/contacts">Контакты</a>
       <a class="p-2 text-muted" href="/posts/create">Создать статью</a>
+      <a class="p-2 text-muted" href="/news/create">Создать новость</a>
+      <a class="p-2 text-muted" href="/news/">Новости</a>
       <a class="p-2 text-muted" href="/admin/feedbacks">Админ. раздел отзывы</a>
       <a class="p-2 text-muted" href="/admin/articles">Админ. раздел статьи</a>
       <a class="p-2 text-muted" href="/tags/create">Создать тег</a>
